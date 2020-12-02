4ed0100a0c06a38a59ecec49bb79e85f8be5336f
Daniel Vrátil <dvratil@kde.org>
2020-12-02 12:32:54 +0100

    Use special Akonadi instance for the flatpak
    Use a dedicated Akonadi instance (called 'flatpak') to avoid
    clash of the exported DBus names with the system-wide Akonadi.
    
    This will only be done for new users that haven't used this
    Flatpak before, as there's no viable migration path from the
    default instance to the Flatpak instance.

--- run_kontact.sh
+++ run_kontact.sh
@@ -1,14 +1,35 @@
-#!/bin/sh
+#!/bin/bash
+#
+# Kontact/Akonadi launch script
+#
+# SPDX-FileCopyrightTexti: 2020 Daniel Vrátil <dvratil@kde.org>
+# SPFX-License-Identifier: GPL-2.0-or-later
+#
 
 function stop_akonadi {
     akonadictl stop
     count=0
-    while [[ $(akonadictl status 2>&1 | grep "running") && $count -lt 5 ]]; do
+    while akonadictl status 2>&1 | grep -q "running" && [ ${count} -lt 5 ]; do
         echo "Waiting for Akonadi to stop..."
-        ((count=$count+1))
+        ((count=count+1))
         sleep 1
     done
 }
+
+export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
+
+# We want to use a dedicated Akonadi instance in the Flatpak in order
+# to avoid the exported DBus names to conflict with the default
+# non-flatpak Akonadi.
+# However, we did not do this from the very beginning of this Flatpak,
+# so we can only use the dedicated instance if the default instance
+# configuration doesn't exist, or in other words if the user ran
+# this Flatpak for the first time after this feature has been introduced.
+# There is no possible migration path from the default instance to the
+# dedicated instance for existing users.
+if [ ! -f "${XDG_CONFIG_HOME}/akonadi/akonadiserverrc" ]; then
+    export AKONADI_INSTANCE="flatpak"
+fi
 
 # Make sure we run against our own Akonadi instance
 stop_akonadi
@@ -28,6 +49,6 @@ kbuildsycoca5
 akonadictl start
 
 # .. aaaaand lift-off
-kontact "$@"
+exec kontact "$@"
 
 
